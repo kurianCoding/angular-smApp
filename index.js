4ff981276b29fe2f095c54c52c540d19e6a94910
kurian <kurian@hifx.co.in>
2017-01-18 16:20:49 +0530

    TIME MARK
    completed: Checking this configuration for git commit is a good thing
    for future also.
    next:

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ app.use('/node_modules',express.static(__dirname+'/node_modules/'));
 app.use('/js',express.static(__dirname+'/templates/js/'));
 app.use('/comments',express.static(__dirname+'/templates/comments/'));
 app.listen(port);
+
