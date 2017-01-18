2522b305f7bdb6347ab6ad6fbb0d2b75fe5a636e
kurian <kurian@hifx.co.in>
2017-01-18 13:58:32 +0530

    added modal traditional way

--- templates/js/app.js
+++ templates/js/app.js
@@ -1,37 +1,50 @@
-angular.module('myApp',[]).directive('myCounter',function(){
-    return{
-	restrict:"EA",
-	replace:true,
-	scope:{},
-	require:'?ngModel',
-	controller:function($scope){
-	    $scope.count= 0;
-	    var update= function (count,increment){
-		return count+increment;
-	    }
-	    $scope.AddFunc=function(){
-		count=update($scope.count,1);
-	    $scope.count=count;
-	    }
-	    $scope.SubtractFunc=function(){
-		count=update($scope.count,-1);
-	    $scope.count=count;
-	    }
-	},
-	link: function(scope,elements,attrs,ngModel){
-	    if(!ngModel){
-		return;
-	    }
-	    ngModel.$render=function(){
-		return elements.html('<div>hello world</div>');
-	    }
-	   },
-	template:`<div>Counter {{count}}
-	    <button  class=\"btn btn-success glyphicon glyphicon-plus\" ng-click=\"AddFunc()\"></button>
-	    <button  class=\"btn btn-warning glyphicon glyphicon-minus\" ng-click=\"SubtractFunc()\"></button>
-</div>`
+angular.module('myApp',[])
+    .directive("myCounter",function(){
+     return{
+	 restrict:"EA",
+	 replace:true,
+	 scope: {},
+	 controller:function($scope){
+	     $scope.count= 0;
+	     var update= function (count,increment){
+		 return count+increment;
+	     }
+	     $scope.addfunc=function(){
+		 count=update($scope.count,1);
+		 $scope.count=count;
+	     }
+	     $scope.subtractfunc=function(){
+		 count=update($scope.count,-1);
+		 $scope.count=count;
+	     }
+	 },
+	 template:`<div>counter {{count}}
+	     <button  class=\"btn btn-success glyphicon glyphicon-plus\" ng-click=\"addfunc()\"></button>
+	     <button  class=\"btn btn-warning glyphicon glyphicon-minus\" ng-click=\"subtractfunc()\"></button>
+	     </div>`
     }
-}).run(function($rootScope){
+})
+    .directive("popupBtn",function() {
+     return {
+	 restrict:"EA",
+	 replace:true,
+	 transclude:true,
+	 scope: {},
+	 controller: function($scope){
+	     return;
+	 },
+	 template:`<div>
+	     <button class=\"btn btn-success\" onClick=\"popupModal()\" ng-transclude>
+	     </button>
+	     </div>`,
+    }
+})
+    .run(function($rootScope){
     console.log("hello world");
 });
+
 angular.bootstrap(document,['myApp']);
+
+function popupModal(){
+$('.modal').modal('show');
+}
