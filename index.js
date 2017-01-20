f7fdc921c8d32e908f1c8f6bb2930758871e0625
kurian <kurian@hifx.co.in>
2017-01-20 16:08:28 +0530

    UPDATE:
    update:adding comment templates.

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 express=require('express');
+var os = require('os');
+
 app=express();
 var port=8800;
 app.use('/',express.static(__dirname+'/templates/'));
 app.use('/node_modules',express.static(__dirname+'/node_modules/'));
 app.use('/js',express.static(__dirname+'/templates/js/'));
 app.use('/comments',express.static(__dirname+'/templates/comments/'));
-app.listen(port);
+app.get('/api',function(req,resp){
+    resp.send({"message":"hello world","status":200});
+});
+app.post('/api',function(req,resp){
+    console.log("post api");
+});
+app.put('/api',function(req,resp){
+    console.log("put api");
+});
 
+
+
+var interfaces = os.networkInterfaces();
+var addresses = [];
+
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+
+console.log(addresses[0]+":"+port);
+
+app.listen(port);
--- templates/js/app.js
+++ templates/js/app.js
@@ -1,10 +1,31 @@
 angular.module('myApp',['ui.bootstrap'])
+    .factory('myInterceptor',function($q){
+	return {
+	    request:function(config){
+		console.log(config.headers);
+		console.log(config.url);
+		return config;
+	    },
+	    response:function(response){
+		return response;
+	    },
+	    requestError:function(rejection){
+		return rejection;
+	    },
+	    responseError: function(rejection){
+		return rejection;
+	    },
+	}
+    })
+    .config(function($httpProvider){
+	$httpProvider.interceptors.push('myInterceptor');
+    })
     .directive("myCounter",function(){
      return{
 	 restrict:"EA",
 	 replace:true,
 	 scope: {},
-	 controller:function($scope){
+	 controller:['$scope','$http',function($scope,$http){
 	     $scope.count= 0;
 	     var update= function (count,increment){
 		 return count+increment;
@@ -12,12 +33,19 @@ angular.module('myApp',['ui.bootstrap'])
 	     $scope.addfunc=function(){
 		 count=update($scope.count,1);
 		 $scope.count=count;
-	     }
+		 $http.get('http://192.168.0.5:8800/api').then(function(response){
+		 });
+		 $http.post('http://192.168.0.5:8800/api').then(function(response){
+		 });
+		 $http.put('http://192.168.0.5:8800/api').then(function(response){
+		 });
+
+}
 	     $scope.subtractfunc=function(){
 		 count=update($scope.count,-1);
 		 $scope.count=count;
 	     }
-	 },
+	 }],
 	 template:`<div>counter {{count}}
 	     <button  class=\"btn btn-success glyphicon glyphicon-plus\" ng-click=\"addfunc()\"></button>
 	     <button  class=\"btn btn-warning glyphicon glyphicon-minus\" ng-click=\"subtractfunc()\"></button>
